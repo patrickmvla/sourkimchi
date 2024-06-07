2e92766e34267d6c6391130f164eebd3af307af7
Patrick Mvula <mvlapatrick@gmail.com>
2024-06-07 04:14:40 +0200

    auth with clerk

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { env } from "./env/client.ts";
+import { ClerkProvider } from "@clerk/clerk-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const PUBLISHABLE_KEY = env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
