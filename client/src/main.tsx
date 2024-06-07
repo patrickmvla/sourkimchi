04393604d901f25a986ffba1efe683992f56f961
Patrick Mvula <mvlapatrick@gmail.com>
2024-06-07 04:40:34 +0200

    initial home setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { env } from "./env/client.ts";
 import { ClerkProvider } from "@clerk/clerk-react";
+
+import "@/index.css";
+import { env } from "@/env/client.ts";
+import { Router } from "@/routes.tsx";
 
 const PUBLISHABLE_KEY = env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,7 +15,7 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <Router />
     </ClerkProvider>
   </React.StrictMode>
 );
