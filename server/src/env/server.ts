a363d20af7da91ea5c93f3ba99a1962ceff54b6b
patrick mvula <mvlapatrick@gmail.com>
2024-06-15 19:07:47 +0200

    db config

--- server/src/env/server.ts
+++ server/src/env/server.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     MONGODB_URL: z.string().url(),
+    NODE_ENV: z.string().min(1),
     SECRET: z.string().min(1)
   },
   runtimeEnv: process.env,
